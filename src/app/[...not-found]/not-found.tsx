3a92e5bb5114f30cd51efc94acd3e355d37e4232
EliasHami <contact@eliashami.com>
2023-06-20 20:03:11 +0200

    workaround for not found in deploy

---
+++ src/app/[...not-found]/not-found.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+// issue : https://github.com/vercel/next.js/issues/51132
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+      <p>
+        View <Link href="/blog">all posts</Link>
+      </p>
+    </div>
+  )
+}
