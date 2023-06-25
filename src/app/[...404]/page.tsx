42f1d0b5ff2778554b8d2c63460b8cdb9e21abb8
EliasHami <contact@eliashami.com>
2023-06-25 19:54:27 +0200

    catch all to fix prod deploy

---
+++ src/app/[...404]/page.tsx
@@ -0,0 +1,10 @@
+
+// issue : https://github.com/vercel/next.js/issues/51132
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+    </div>
+  )
+}
